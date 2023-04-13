29c2d28aa8283ff3ee7a40fb53ba8ee6a72ebb4b
margarethcordova <aurea.margareth.cs@gmail.com>
2023-04-13 11:01:23 -0500

    actualizacion <3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
     alert('cargo la pagina completamente');
     $('#loading').fadeOut();
     $('body').removeClass('hidden');
@@ -9,18 +9,19 @@ $(document).ready(function () {
         $('.menu-burger').toggleClass('active')
         $('nav').toggleClass('active')
     })
+
 });
 
-//cart
-let cartIcon = document.querySelector("cart-icon");
-let cart = document.querySelector(".cart")
-let closeCart= document.querySelector("#close-cart")
+// //cart
+// let cartIcon = document.querySelector("cart-icon");
+// let cart = document.querySelector(".cart")
+// let closeCart= document.querySelector("#close-cart")
 
-//open cart
-carTIcon.onclick =() => {
-    cart.classList.add("active");
-};
-//close cart
-closeCart.onclick = () =>{
-    cart.classList.remove("active");
-};
+// //open cart
+// carTIcon.onclick =() => {
+//     cart.classList.add("active");
+// };
+// //close cart
+// closeCart.onclick = () =>{
+//     cart.classList.remove("active");
+// };

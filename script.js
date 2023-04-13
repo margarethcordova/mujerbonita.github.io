aa8379d8dd8fc4478e328eb563e63f910985e66f
Evilsanchez <evelinsanchezvelasquez11@gmail.com>
2023-04-12 21:27:07 -0400

    shop-actu

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ $(document).ready(function () {
         $('nav').toggleClass('active')
     })
 });
+
+//cart
+let cartIcon = document.querySelector("cart-icon");
+let cart = document.querySelector(".cart")
+let closeCart= document.querySelector("#close-cart")
+
+//open cart
+carTIcon.onclick =() => {
+    cart.classList.add("active");
+};
+//close cart
+closeCart.onclick = () =>{
+    cart.classList.remove("active");
+};

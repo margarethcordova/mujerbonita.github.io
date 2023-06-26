e5f870a508ec4cef2ba42dc8a2efae9753b834c9
margarethcordova <aurea.margareth.cs@gmail.com>
2023-06-26 03:25:55 -0500

    actualizacion

--- js/app.js
+++ js/app.js
@@ -15,12 +15,13 @@ closeCart.onclick = () => {
 if (document.readyState == 'loading') {
     document.addEventListener('DOMcontentLoaded', ready)
 } else {
-    ready
+    ready();
 }
+
 //making funtion
 function ready() {
     //reomve itemsfrom cart
-    var reomveCardButtons = document.getElementsByClassName('cart-remove');
+    var reomveCardButtons = document.getElementsByClassName("cart-remove");
     console.log(reomveCardButtons);
     for (var i = 0; i < reomveCardButtons.length; i++) {
         var button = reomveCardButtons[i];
@@ -28,11 +29,11 @@ function ready() {
 
     }
     //quantity changes
-var quantityInputs = document.getElementsByClassName('cart-quantity')
-for (var i = 0; i < quantityInputs.length; i++){
-    var input = quantityInputs[i]
-    input.addEventListener('change',quantitychanged)
-}
+    var quantityInputs = document.getElementsByClassName('cart-quantity')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantitychanged)
+    }
 }
 
 //remove itemsfrom cart
@@ -43,15 +44,16 @@ function removeCardItem(event) {
 }
 
 //quantity changes
-function quantitychanged(event){
-    var input = event.target
-    if (NaN(input.value) || input.value <= 0{
-        input.value = 1
-    })
+function quantitychanged(event) {
+    var input = event.target;
+    if (NaN(input.value) || input.value <= 0){
+        input.value = 1;
+    };
 }
+
 //update total
 function updatetotal() {
-    var cartcontenet = document.getElementsByClassName("cart-content")[0];
+    var cartcontenet = document.getElementsByClassName("cart-contenet")[0];
     var cartBoxes = document.getElementsByClassName("cart-box");
     var total = 0;
     for (var i = 0; i < cartBoxes.length; i++) {
